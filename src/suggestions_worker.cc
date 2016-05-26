7899cd2081f85a2b5b29ece47743d83dba732314
Pascal Hertleif <killercup@gmail.com>
2016-05-26 18:07:10 +0200

    Async Stuff
    Needs to be synchronized!

---
+++ src/suggestions_worker.cc
@@ -0,0 +1,70 @@
+#include "ZHfstOspeller.h"
+#include "ospell.h"
+#include <nan.h>
+
+class SuggestionsWorker : public Nan::AsyncWorker {
+public:
+  SuggestionsWorker(Nan::Callback *callback, ZHfstOspeller *speller,
+                    std::string word)
+      : Nan::AsyncWorker(callback), speller(speller), word(word) {}
+  ~SuggestionsWorker() {}
+
+  // Executed inside the worker-thread.
+  // It is not safe to access V8, or V8 data structures here, so everything we
+  // need for input and output should go on `this`.
+  void Execute() {
+    // Check spelling
+    correct_word = speller->spell(word);
+    if (correct_word) {
+      return;
+    }
+
+    // Save suggestions
+    corrections = speller->suggest(word);
+  }
+
+  // Executed when the async work is complete this function will be run inside
+  // the main event loop so it is safe to use V8 again
+  void HandleOKCallback() {
+    v8::Isolate *isolate = v8::Isolate::GetCurrent();
+
+    if (correct_word) {
+      v8::Local<v8::Value> argv[] = {Nan::Null(),
+                                     v8::Boolean::New(isolate, false)};
+
+      callback->Call(2, argv);
+      return;
+    }
+
+    v8::Handle<v8::Array> correctionsArray =
+        v8::Array::New(isolate, corrections.size());
+
+    // Return an empty result if there was an error creating the array.
+    if (correctionsArray.IsEmpty()) {
+      v8::Local<v8::Value> argv[] = {Nan::Error("Error creating the arry"),
+                                     Nan::Null()};
+
+      callback->Call(2, argv);
+      return;
+    }
+
+    // Populate the array
+    unsigned int i = 0;
+    while (corrections.size() > 0) {
+      const std::string &corr = corrections.top().first;
+      correctionsArray->Set(i, v8::String::NewFromUtf8(isolate, corr.c_str()));
+      corrections.pop();
+      i++;
+    }
+
+    v8::Local<v8::Value> argv[] = {Nan::Null(), correctionsArray};
+
+    callback->Call(2, argv);
+  }
+
+private:
+  std::string word;
+  ZHfstOspeller *speller;
+  hfst_ol::CorrectionQueue corrections;
+  bool correct_word;
+};
--- test.js
+++ test.js
@@ -2,34 +2,58 @@ import test from 'ava';
 import {SpellChecker} from "./";
 
 test("constructor works", (t) => {
-    const spellchecker = new SpellChecker("etc/se.zhfst");
+  const spellchecker = new SpellChecker("etc/se.zhfst");
 
-    t.throws(() => new SpellChecker(), /wrong number of arguments/i);
-    t.throws(() => new SpellChecker(2), /should be a string/i);
+  // t.throws(() => SpellChecker(), /Cannot call (\w*) as a function/i);
+  t.throws(() => new SpellChecker(), /wrong number of arguments/i);
+  t.throws(() => new SpellChecker(2), /should be a string/i);
 });
 
-test("spell check signature", (t) => {
-    const spellchecker = new SpellChecker("etc/se.zhfst");
+test("spell check signature", async (t) => {
+  const spellchecker = new SpellChecker("etc/se.zhfst");
+  t.plan(2);
 
-    t.throws(() => spellchecker.suggestions(), /wrong number of arguments/i);
-    t.throws(() => spellchecker.suggestions(2), /should be a string/i);
+  spellchecker.suggestions()
+    .catch(err => t.regex(err.message, /first argument should be a string/i));
+
+  spellchecker.suggestions(2)
+    .catch(err => t.regex(err.message, /first argument should be a string/i));
 });
 
-test("no suggestions for correct word", (t) => {
-    const spellchecker = new SpellChecker("etc/se.zhfst");
+test("no suggestions for correct word", async (t) => {
+  const spellchecker = new SpellChecker("etc/se.zhfst");
+  const suggestions = await spellchecker.suggestions("Lákku");
 
-    t.deepEqual(spellchecker.suggestions("Lákku"), false);
+  t.deepEqual(suggestions, false);
 });
 
-test("spelling suggestions", (t) => {
-    const spellchecker = new SpellChecker("etc/se.zhfst");
+test("spelling suggestions", async (t) => {
+  const spellchecker = new SpellChecker("etc/se.zhfst");
+  const suggestions = await spellchecker.suggestions("akkusativa");
 
-    t.deepEqual(spellchecker.suggestions("akkusativa"),
-        ['akkusatiivva', 'akkusatiiva', 'akkusatiivan']);
+  t.deepEqual(suggestions, ['akkusatiivva', 'akkusatiiva', 'akkusatiivan']);
 });
 
-test("no spelling suggestions possible", (t) => {
-    const spellchecker = new SpellChecker("etc/se.zhfst");
+test("no spelling suggestions possible", async (t) => {
+  const spellchecker = new SpellChecker("etc/se.zhfst");
+  const suggestions = await spellchecker.suggestions("apfelkuchen");
 
-    t.deepEqual(spellchecker.suggestions("apfelkuchen"), []);
+  t.deepEqual(suggestions, []);
 });
+
+// test("concurrent spelling suggestions", async (t) => {
+//   const spellchecker = new SpellChecker("etc/se.zhfst");
+//   const suggestions = await Promise.all([
+//     spellchecker.suggestions("akkusativa"),
+//     spellchecker.suggestions("qwert"),
+//     spellchecker.suggestions("nutella"),
+//     spellchecker.suggestions("straßenbahn"),
+//   ]);
+
+//   t.deepEqual(suggestions, [
+//     ['akkusatiivva', 'akkusatiiva', 'akkusatiivan'],
+//     ['Bert', 'Evert', 'Mweru', 'Zwart', 'Uwet', 'Kert', 'Owet', 'Gjert', 'Egert', 'Gert'],
+//     ['Iuella', 'Puteola', 'njiella', 'Uutela', 'nulla', 'Sutela', 'Juella', 'Stella'],
+//     [],
+//   ]);
+// });
